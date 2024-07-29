256a25e538281f5b221791e4e2b4bdb076565001
AnindyaBsws <shakhar004@mail.com>
2024-07-29 12:05:17 -0700

    Pattern problem added

--- SquarePat.java
+++ SquarePat.java
@@ -40,23 +40,23 @@
 // _____Hollow pattern for rectangular shape____
 
 
-public class SquarePat {
-    public static void main(String args[]) {
-        int n = 6;
-        int m = 13;
-        for(int i=1; i<=n ; i++) {
-            for(int j=1; j<=m; j++) {
-                if (i == 3 & j == 6 || i == 3 & j == 7 || i == 3 & j == 8 || i == 4 & j == 6 || i == 4 & j == 7 || i == 4 & j == 8 ) {
-                    System.out.print(" ");
-                }
-                else{
-                    System.out.print("*");
-                }
-            }
-            System.out.println();
-        }
-    }
+// public class SquarePat {
+//     public static void main(String args[]) {
+//         int n = 6;
+//         int m = 13;
+//         for(int i=1; i<=n ; i++) {
+//             for(int j=1; j<=m; j++) {
+//                 if (i == 3 & j == 6 || i == 3 & j == 7 || i == 3 & j == 8 || i == 4 & j == 6 || i == 4 & j == 7 || i == 4 & j == 8 ) {
+//                     System.out.print(" ");
+//                 }
+//                 else{
+//                     System.out.print("*");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
     
-}
+// }
 
 
